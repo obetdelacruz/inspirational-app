91442ed5c6a27ba0091eedd18d7a6682592806a1
obetdelacruz <ldelacruz@gbox.adnu.edu.ph>
2023-04-23 22:21:18 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+/*
+import "./style.css";
+
+function getQuote() {
+  return new Promise((resolve, reject) => {
+    fetch("https://api.quotable.io/quotes/random")
+      .then((response) => response.json())
+      .then((data) => resolve(data))
+      .catch((error) => reject(error));
+  });
+}
+getQuote()
+  .then((data) => {
+    document.body.innerHTML = `<h1>${data[0].author}</h1>
+      <p>${data[0].content}</p>`;
+    document;
+  })
+  .catch((error) => console.error(error));
+*/
